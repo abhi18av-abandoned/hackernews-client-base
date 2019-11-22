34f3359b8b8a18960ea27ddb631a9a02b7138f35
Abhinav Sharma <abhi18av@gmail.com>
2019-11-22 21:45:03 +0530

    render the initial state via useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const App = () => {
 
     const [storyIds, setStoryIds] = useState([]);
 
+    useEffect(() => {
+        setStoryIds('Hello from useEffect!');
+    }, []);
+
+
     return (
-        <p>Hello, HackerNews!</p>
+        <p>{storyIds}</p>
     );
 
 };
