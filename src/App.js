d74bbddd6a383bb2f9d5d0f57e94eb61d35ff6aa
Abhinav Sharma <abhi18av@gmail.com>
2019-11-23 13:02:10 +0530

    restart the app on a single file basis

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import {StoriesContainer} from "./containers/StoriesContainers";
 
 export const App = () => {
 
     return (
-        <StoriesContainer/>
+        <p>Hello, HackerNews!</p>
     );
 
 };
